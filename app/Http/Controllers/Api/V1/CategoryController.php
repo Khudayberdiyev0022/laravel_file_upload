d1a3ae671eda83b0030ecd3c5351675059c18fa8
khamza <khamza.khudayberdiyev@gmail.com>
2024-05-22 12:53:37 +0500

    categories

---
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    $categories = Category::all();
+
+    return response()->json($categories);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreCategoryRequest $request)
+  {
+    return Category::query()->create($request->all());
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(Category $category)
+  {
+    return response()->json($category);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateCategoryRequest $request, Category $category)
+  {
+    $category->update($request->all());
+
+    return response()->json($category);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Category $category)
+  {
+    $category->delete();
+
+    return response()->json(['message' => 'Category deleted!']);
+  }
+}
